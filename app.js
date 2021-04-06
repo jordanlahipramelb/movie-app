7bb0f9e55e75abba879df0f5906cb6cc2b3e8cb6
jordanlahipramelb <jordan.lahipramelb@gmail.com>
2021-04-06 16:54:59 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+$(function () {
+  //EVENT
+  $('#movie-form').on('submit', function (evt) {
+    evt.preventDefault();
+
+    let title = $('#title').val();
+    let rating = $('#rating').val();
+    let movieData = { title, rating };
+
+    //create HTML structure with data entered
+    const htmlToAdd = createMovieHTMLData(movieData);
+
+    $('#movie-list').append(htmlToAdd);
+
+    $('#movie-form').trigger('reset');
+  });
+
+  //EVENT
+
+  //THIS WILL REMOVE THE DATA WHEN YOU ALSO CLICK THE DATA ITSELF
+
+  //   $('tbody').on('click', 'tr', function () {
+  //     $(this).remove();
+  //   });
+
+  //DELETES WHEN YOU CLICK THE BUTTON
+  $('tbody').on('click', '.delete', function (evt) {
+    $(evt.target).closest('tr').remove();
+  });
+});
+
+//create HTML structure with data entered
+function createMovieHTMLData(obj) {
+  return `
+      <tr>
+      
+        <td>${obj.title}</td>
+        <td>${obj.rating}</td>
+        <td>
+          <button class="delete">
+            Delete
+          </button>
+        </td>
+      <tr>`;
+}
